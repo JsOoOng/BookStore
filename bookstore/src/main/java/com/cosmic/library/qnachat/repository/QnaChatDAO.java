8619758d49bc7af0c012f0f0643bea4d92ed86fd
JsOoOng <qkr030503@naver.com>
2026-03-25 20:42:01 +0900

    Update QnaChatDAO.java

--- bookstore/src/main/java/com/cosmic/library/qnachat/repository/QnaChatDAO.java
+++ bookstore/src/main/java/com/cosmic/library/qnachat/repository/QnaChatDAO.java
@@ -6,5 +6,6 @@ import java.util.List;
 public interface QnaChatDAO {
     void insertMessage(QnachatVO vo);
     // 과거 내역 조회를 위한 메서드 추가 (권한에 따라 다르게 조회)
+    // 백업용
     List<QnachatVO> getChatHistory(String userId, String role);
 }
